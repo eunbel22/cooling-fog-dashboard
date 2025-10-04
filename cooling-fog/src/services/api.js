d2d0ea64506441a4469b5343c3e94319665d5e73
eunbel22 <134656323+eunbel22@users.noreply.github.com>
2025-10-04 11:43:20 +0900

    Update api.js
    백엔드 연결 바꾸기

--- cooling-fog/src/services/api.js
+++ cooling-fog/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8050';
+const BASE_URL = process.env.REACT_APP_API_BASE || 'http://localhost:8050';
+
 
 const api = axios.create({
   baseURL: BASE_URL,
